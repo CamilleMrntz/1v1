a0ff7aeea3157783349a67ad325fcb72c3d946f0
CamilleMrntz <94170738+CamilleMrntz@users.noreply.github.com>
2022-05-29 22:22:46 +0200

    Changes done
    Changes on the player commandes

---
+++ one_v_one_v2/one_v_one_v2/game/game.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>1 v 1</title>
+</head>
+
+<body>
+    <div class="hp">
+        <p id="healthPlayer" class="hp_player"></p>
+        <p id="healthEnemy" class="hp_player"></p>
+    </div>
+
+    <canvas id="canvas"><canvas>
+
+    <div class="game-over">
+        <h1>Bim</h1>
+        <h2 class="score"></h2>
+        <button class="btn" onclick="window.location.reload()">Restart</button>
+        <button class="btn" onclick="window.location.href='../home.php'">Home</button>
+    </div>
+
+
+    <script src="js/game.js"></script>
+</body>
+
+</html>
